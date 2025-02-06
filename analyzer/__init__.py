ee3c492be3ee37ff0b211e339d14c6474c25403a
Cédric Brunet <brunetced42@gmail.com>
2025-02-06 09:57:32 +0100

    Revert "CsvParseur"
    This reverts commit 0b3eb1c1b1d74986e5c4cc0c6104c6bf8e528a71.

--- analyzer/__init__.py
+++
@@ -1,6 +0,0 @@
-from .csv_analyzer import CSVAnalyzer
-from .csv_parseur import CSVProductParseur
-from .report_generator import ReportGenerator
-
-__all__ = ["CSVAnalyzer", "CSVProductParseur", "ReportGenerator"]
-
